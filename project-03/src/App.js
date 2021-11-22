0c01e99118e50ca1a3aa419c776ae0638fbd6e9a
sh3ngsh3ng <yshenglim13@gmail.com>
2021-11-22 07:50:14 +0000

    added carousel to application

--- project-03/src/App.js
+++ project-03/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import FeaturedCarousel from './components/Carousel'
+import NavBar from "./components/NavBar"
+import React from 'react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NavBar />
+      <FeaturedCarousel />
+      <h1>This is the Body Content</h1>
+    </React.Fragment>
+    
   );
 }
 
---
+++ project-03/src/images/index.js
@@ -0,0 +1,11 @@
+// file to export images
+
+import sample01 from './carousel-images/sample01.jpg'
+import sample02 from './carousel-images/sample02.jpg'
+
+export {
+    sample01,
+    sample02
+}
+
+
