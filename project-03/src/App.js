dcd7ca95bd6e155246a8a67ef54e4738cc416407
sh3ngsh3ng <yshenglim13@gmail.com>
2021-11-23 09:59:52 +0000

    added routing in App.js

--- project-03/src/App.js
+++ project-03/src/App.js
@@ -2,16 +2,32 @@ import './App.css';
 import FeaturedCarousel from './components/Carousel'
 import NavBar from "./components/NavBar"
 import React from 'react';
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 
 
 function App() {
   return (
+    <Router>
     <React.Fragment>
       <NavBar />
-      <FeaturedCarousel />
-      <h1>This is the Body Content</h1>
+      <Switch>
+        <Route exact path="/">
+          <FeaturedCarousel />
+          <h1>Product Listings</h1>
+          <h1>Reviews</h1>
+        </Route>
+        <Route exact path="/contact-us">
+          <h1>You can contact us here!</h1>
+        </Route>
+        <Route exact path="/login">
+          <h1>You can login here!</h1>
+        </Route>
+        <Route exact path="/sign-up">
+          <h1>You can sign up here!</h1>
+        </Route>
+      </Switch>
     </React.Fragment>
+    </Router>
     
   );
 }
