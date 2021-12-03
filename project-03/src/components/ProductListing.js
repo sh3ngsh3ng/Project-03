84d47a6cacb65078e8a51a7e40e0fec89b314fa6
sh3ngsh3ng <yshenglim13@gmail.com>
2021-12-03 08:30:56 +0000

    added list rendering of active listings

--- project-03/src/components/ProductListing.js
+++ project-03/src/components/ProductListing.js
@@ -1,47 +1,74 @@
-import React from 'react'
-import {Card, Button} from 'react-bootstrap'
-import {spidey01, spidey02, spidey03} from "../images"
+import {React, useContext, useEffect, useState} from 'react'
+// import {Card, Button} from 'react-bootstrap'
+import ProductContext from "./ProductContext"
+// import {spidey01, spidey02, spidey03} from "../images"
+
+
 
 export default function ProductListing() {
 
-
+    const [activeListings, setActiveListings] = useState([])
+    const context = useContext(ProductContext)
+    
+    
+    context.getActiveListings().then(res => setActiveListings(res))
 
     return (
         <div>
-            {/* list rendering of cards here */}
-            <Card style={{ width: '18rem', display:"inline-block"}}>
-                <Card.Img variant="top" src={spidey01} />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem', display:"inline-block" }}>
-                <Card.Img variant="top" src={spidey02} />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
-            <Card style={{ width: '18rem', display:"inline-block" }}>
-                <Card.Img variant="top" src={spidey03} />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+            <ul>
+                {activeListings.map((listing) => (
+                    <li>{listing.product_name}</li>
+                ))}
+            </ul>
         </div>
     )
+
+
 }
+
+
+
+
+
+
+// TESTING
+// export default function ProductListing() {
+//     return (
+//         <div>
+//             {/* list rendering of cards here */}
+//             <Card style={{ width: '18rem', display:"inline-block"}}>
+//                 <Card.Img variant="top" src={spidey01} />
+//                 <Card.Body>
+//                     <Card.Title>Card Title</Card.Title>
+//                     <Card.Text>
+//                         Some quick example text to build on the card title and make up the bulk of
+//                         the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Go somewhere</Button>
+//                 </Card.Body>
+//             </Card>
+//             <Card style={{ width: '18rem', display:"inline-block" }}>
+//                 <Card.Img variant="top" src={spidey02} />
+//                 <Card.Body>
+//                     <Card.Title>Card Title</Card.Title>
+//                     <Card.Text>
+//                         Some quick example text to build on the card title and make up the bulk of
+//                         the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Go somewhere</Button>
+//                 </Card.Body>
+//             </Card>
+//             <Card style={{ width: '18rem', display:"inline-block" }}>
+//                 <Card.Img variant="top" src={spidey03} />
+//                 <Card.Body>
+//                     <Card.Title>Card Title</Card.Title>
+//                     <Card.Text>
+//                         Some quick example text to build on the card title and make up the bulk of
+//                         the card's content.
+//                     </Card.Text>
+//                     <Button variant="primary">Go somewhere</Button>
+//                 </Card.Body>
+//             </Card>
+//         </div>
+//     )
+// }
--- project-03/src/components/ProductSection.js
+++ project-03/src/components/ProductSection.js
@@ -2,7 +2,7 @@ import {React, useState} from 'react'
 import ProductListing from "./ProductListing"
 import ProductListingTab from "./ProductListingTab"
 import ProductContext from './ProductContext'
-
+import axios from "axios"
 
 export default function ProductSection() {
 
@@ -12,6 +12,10 @@ export default function ProductSection() {
         'activeTab': activeTab,
         'setActiveTab': (evt) => {
             setActiveTab(evt.target.value)
+        },
+        'getActiveListings': async () => {
+            let response = await axios.get("https://3000-amber-guppy-qbo1ebq4.ws-us21.gitpod.io/api/products/active-listings")
+            return response.data
         }
     }
 
