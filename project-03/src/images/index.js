07824c0bacc1ac303488502409880bd8a9fba801
sh3ngsh3ng <yshenglim13@gmail.com>
2021-12-13 09:34:56 +0000

    saved

--- project-03/src/images/index.js
+++ project-03/src/images/index.js
@@ -1,19 +1,14 @@
 // file to export images
 
-import sample01 from './carousel-images/sample01.jpg'
-import sample02 from './carousel-images/sample02.jpg'
+
 import userLogo from './logo/user-logo.png'
 import brandLogo from './logo/brand-logo.png'
 import brandLogo2 from "./logo/brand-logo2.png"
-import bannerImage from "./carousel-images/banner-image.jpg"
 
 export {
-    sample01,
-    sample02,
     userLogo,
     brandLogo,
-    brandLogo2,
-    bannerImage
+    brandLogo2
 }
 
 
