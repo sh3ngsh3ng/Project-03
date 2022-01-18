8b0ca429bf7b60755e9a48b6f2d20196cae3213c
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-18 11:30:03 +0000

    styled orders page to 468,725,1024

--- project-03/src/components/OrderHistory.js
+++ project-03/src/components/OrderHistory.js
@@ -21,20 +21,23 @@ export default function OrderHistory() {
         <React.Fragment>
             {data.map((order) => {
                 return (
-                    <div className="container" style={{"background": "grey", "margin-bottom": "10px", "padding": "10px"}}>
-                        <p>Order Date: {order.order_date}</p>
-                        <p>Total Cost: {order.order_total_cost}</p>
+                    <div className="container order-history-item-div" >
+                        <p className="pending-order-text">Order Date: {order.order_date.slice(0, 10)}</p>
+                        <p className="pending-order-text">Total Cost: ${order.order_total_cost / 100}</p>
                         <ol className="list-group list-group-numbered">
                             {order.orderitems.map((item) => {
                                 return (
-                                    <li className="list-group-item history justify-content-between align-items-start" style={{"background": "pink", "display": "flex"}}>
+                                    <li className="list-group-item order-history-item" style={{"background": "pink", "display": "flex"}}>
                                         
                                         <div className="ms-2 me-auto">
-                                            <div className="fw-bold">{item.productslot.product.product_name}</div>
-                                            <div>Slot Time: {item.productslot.slot_datetime.slice(0, 10) + " ,"} {item.productslot.slot_datetime.slice(11, 16)}</div>
-                                            <div>Order Status: {item.order_item_status.charAt(0).toUpperCase() + item.order_item_status.slice(1)}</div>
+                                            <div className="fw-bold pending-order-text">{item.productslot.product.product_name}</div>
+                                            <div className="pending-order-text">Slot Time: {item.productslot.slot_datetime.slice(0, 10) + " ,"} {item.productslot.slot_datetime.slice(11, 16)}</div>
+                                            <div className="pending-order-text">Order Status: {item.order_item_status.charAt(0).toUpperCase() + item.order_item_status.slice(1)}</div>
                                         </div>
-                                        <span>Qty x {item.order_item_quantity}</span>
+                                        <div>
+                                            <span className="pending-order-text">Qty x {item.order_item_quantity}</span>
+                                        </div>
+                                        
                                     </li>
                                 )
                             })}
