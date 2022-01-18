fa8edb699152d656d26ed6443ebc9237c7d7afc8
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-18 03:24:43 +0000

    conditional rendering of order items in PendingOrders

--- project-03/src/components/PendingOrders.js
+++ project-03/src/components/PendingOrders.js
@@ -10,6 +10,7 @@ export default function PendingOrders() {
     useEffect(() => {
         async function getData() {
             let response = await context.getPendingOrders()
+            console.log(response.data)
             setData(response.data)
         }
         getData()
@@ -18,8 +19,20 @@ export default function PendingOrders() {
     // context.getPendingOrders().then((results) => setData(results.data))
     return (
         <React.Fragment>
-            {data.map(function(each) {
-                return <h1>{each.id}</h1>
+            {data.map((item) => {
+                return (
+                    <div className="container" style={{"border": "1px black solid"}}>
+                        <p>Product Name: {item.productslot.product.product_name}</p>
+                        <p>Big image: {item.productslot.product.image_url}</p>
+                        <p>Thumbnail: {item.productslot.product.thumbnail_url}</p>
+                        <p>Roomtype: {item.productslot.product.room_type}</p>
+                        <p>Price: ${item.productslot.product.product_price / 100}</p>
+                        <p>Datetime: {item.productslot.slot_datetime}</p>
+                        <p>Order Status: {item.order.order_status}</p>
+                        <p>Payment Status: {item.order_item_status}</p>
+                        <p>Quantity: {item.order_item_quantity}</p>
+                    </div>
+                )
             })}
             "PendingOrder Component"
         </React.Fragment>
