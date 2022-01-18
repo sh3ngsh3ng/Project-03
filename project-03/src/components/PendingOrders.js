54e1af604e69e1aced5c1b46baa8f56a8482a826
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-18 07:05:03 +0000

    styled pending orders

--- project-03/src/components/PendingOrders.js
+++ project-03/src/components/PendingOrders.js
@@ -23,23 +23,23 @@ export default function PendingOrders() {
             {data.map((item) => {
                 return (
                     <div className= "container pending-order-item">
-                        <div style={{"display": "flex", "justify-content": "space-between", "margin-bottom": "10px"}}>
+                        <div className="pending-order-header-div">
                             <span className="pending-order-text">{item.productslot.product.room_type}</span>
                             <span className="pending-order-text">Payment Status: {item.order.order_status}</span>
                         </div>
                         {/* <p>Big image: {item.productslot.product.image_url}</p> */}
                         {/* <img src={item.productslot.product.image_url}/> */}
                         {/* <p>Thumbnail: {item.productslot.product.thumbnail_url}</p> */}
-                        <div className="d-flex" style={{"margin-bottom": "10px"}}>
-                            <img style={{"height": "100%"}} src={item.productslot.product.thumbnail_url} style={{"margin-right": "20px"}}/>
+                        <div className="pending-order-body-div">
+                            <img className="pending-order-img" src={item.productslot.product.thumbnail_url} style={{"margin-right": "20px"}}/>
                             <div>
-                                <p className="pending-order-text" style={{"margin": "0"}}>Room: {item.productslot.product.product_name}</p>
-                                <p className="pending-order-text" style={{"margin": "0"}}>Price: ${item.productslot.product.product_price / 100}</p>
-                                <p className="pending-order-text" style={{"margin": "0"}}>Quantity: {item.order_item_quantity}</p>
+                                <p className="pending-order-text">Room: {item.productslot.product.product_name}</p>
+                                <p className="pending-order-text">Price: ${item.productslot.product.product_price / 100}</p>
+                                <p className="pending-order-text">Quantity: {item.order_item_quantity}</p>
                             </div>
                         </div>
 
-                        <div style={{"display": "flex","justify-content": "space-between"}}>
+                        <div className="pending-order-bottom-div">
                             <span className="pending-order-text">Order Date: {item.productslot.slot_datetime.slice(0, 10) + " "} </span>
                             <span className="pending-order-text">Order Status: {item.order_item_status}</span>
                         </div>
