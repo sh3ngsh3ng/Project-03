ac6d8a9d8905075c64a7e186aadd409527f69dcf
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-19 20:15:49 +0000

    added conditional rendering of button for image

--- project-03/src/components/RoomSlider.js
+++ project-03/src/components/RoomSlider.js
@@ -32,6 +32,15 @@ export default class RoomSlider extends Component {
             // variableWidth: true,
             responsive: [
                 {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 1,
+                        dots: false,
+                        swipeToSlide: true
+                    }
+                },
+                {
                     breakpoint: 1024,
                     settings: {
                         slidesToShow: 5,
--- project-03/src/components/SliderCard.js
+++ project-03/src/components/SliderCard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState} from "react"
 import { useSpring, animated } from 'react-spring'
 
 
@@ -16,6 +16,19 @@ export default function SliderCard(props) {
         },
         immediate: key => key === "zIndex"
     }))
+
+    const [displayButton, setDisplayButton] = useState(false)
+    const showButton = (e) => {
+        e.preventDefault()
+        console.log("working")
+        setDisplayButton(true)
+    }
+
+    const hideButton = (e) => {
+        e.preventDefault()
+        setDisplayButton(false)
+    }
+
 
     return(
         <React.Fragment>
@@ -45,21 +58,26 @@ export default function SliderCard(props) {
                 <img className="img-slide" src={props.data.image_url} />
             </animated.div> */}
             <div className="img-slide-div">
+                <button className={"btn btn-sm btn-primary " + (displayButton ? "d-flex": "d-none")}
+                style={{"position": "absolute", "zIndex": "102344"}}
+                >test</button>
                 <animated.img 
-                onMouseLeave={() => {
+                onMouseLeave={(e) => {
                     setSpring({
                         scale: 1,
                         boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.62)",
                         zIndex:0
                     })
+                    hideButton(e)
                 }}
-                onMouseEnter={() => {
+                onMouseEnter={function(e) {
                     setSpring({
                         scale: 1.5,
                         boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.42)",
                         zIndex: 10,
                         opacity: 1,
                     })
+                    showButton(e)
                 }}
                 style={{
                     transform: spring.scale.to(
@@ -69,6 +87,7 @@ export default function SliderCard(props) {
                     zIndex: spring.zIndex
                 }}
                 className="img-slide" src={props.data.image_url} />
+                
             </div>
         </React.Fragment>
 
