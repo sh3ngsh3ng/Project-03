25db8a27b985092f1227b4aa0f7ea49439895ca2
sh3ngsh3ng <yshenglim13@gmail.com>
2022-01-19 20:23:22 +0000

    added rendering of product title

--- project-03/src/components/SliderCard.js
+++ project-03/src/components/SliderCard.js
@@ -20,7 +20,6 @@ export default function SliderCard(props) {
     const [displayButton, setDisplayButton] = useState(false)
     const showButton = (e) => {
         e.preventDefault()
-        console.log("working")
         setDisplayButton(true)
     }
 
@@ -32,34 +31,11 @@ export default function SliderCard(props) {
 
     return(
         <React.Fragment>
-            {/* <animated.div className="img-slide-div"
-            onMouseLeave={() => {
-                setSpring({
-                    scale: 1,
-                    boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.62)",
-                    zIndex:0
-                })
-            }}
-            onMouseEnter={() => {
-                setSpring({
-                    scale: 1.5,
-                    boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.42)",
-                    zIndex: 10,
-                    opacity: 1,
-                })
-            }}
-            style={{
-                transform: spring.scale.to(
-                    s => `scale(${s}) rotateZ(.1deg)`
-                ),
-                boxShadow: spring.boxShadow,
-                zIndex: spring.zIndex
-            }}>
-                <img className="img-slide" src={props.data.image_url} />
-            </animated.div> */}
+            
             <div className="img-slide-div">
+                <span className={displayButton ? "d-flex": "d-none"} style={{"position": "absolute", "zIndex": "100", "color": "white"}}>{props.data.product_name}</span>
                 <button className={"btn btn-sm btn-primary " + (displayButton ? "d-flex": "d-none")}
-                style={{"position": "absolute", "zIndex": "102344"}}
+                style={{"position": "absolute", "zIndex": "100", "bottom": "0", "right": "0"}}
                 >test</button>
                 <animated.img 
                 onMouseLeave={(e) => {
@@ -96,3 +72,32 @@ export default function SliderCard(props) {
 
 
 }
+
+
+
+
+{/* <animated.div className="img-slide-div"
+            onMouseLeave={() => {
+                setSpring({
+                    scale: 1,
+                    boxShadow: "3px 3px 5px rgba(0, 0, 0, 0.62)",
+                    zIndex:0
+                })
+            }}
+            onMouseEnter={() => {
+                setSpring({
+                    scale: 1.5,
+                    boxShadow: "5px 5px 10px rgba(0, 0, 0, 0.42)",
+                    zIndex: 10,
+                    opacity: 1,
+                })
+            }}
+            style={{
+                transform: spring.scale.to(
+                    s => `scale(${s}) rotateZ(.1deg)`
+                ),
+                boxShadow: spring.boxShadow,
+                zIndex: spring.zIndex
+            }}>
+                <img className="img-slide" src={props.data.image_url} />
+            </animated.div> */}
